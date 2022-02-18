760a8bb179259c25032fce43a986103541f68951
Dleite2008 <99999757+Dleite2008@users.noreply.github.com>
2022-02-18 17:00:06 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+var box;
+function setup() {
+  createCanvas(400,400);
+  box =createSprite(200,200,50,50);
+}
+
+function draw() 
+{
+  background(220);
+  if (keyDown("space")){
+    box.y-=5;
+  }
+drawSprites();
+}
+
+
+
+
